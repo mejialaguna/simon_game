c705a149d4817fdb9bea60f10bb6da4947ed9cd8
Jose Mejia <mejialaguna@yahoo.com>
2021-12-02 21:41:56 -0500

    userPattern

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-gamePattern= []
+const gamePattern = [];
+const userClickedPattern = [];
+const btnColor = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-    const btnColor = ["red", "blue", "green", "yellow"];
-    const randomNumber = Math.floor(Math.random() * 4);
-    const randomChosenColor = btnColor[randomNumber];
-    gamePattern.push(randomChosenColor)
-    console.log(randomNumber, btnColor[randomNumber]);
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColor = btnColor[randomNumber];
+  gamePattern.push(randomChosenColor);
+  //   console.log(randomNumber, randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  new Audio(`sounds/${randomChosenColor}.mp3`).play();
 }
-
-$(document).keypress(function () {
-  $("h1").text("Level");
+function handler() {
+  $(".btn").click(function () {
+    nextSequence();
+    const userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor)
+    console.log(userClickedPattern)
 });
+}
+handler();
+
+// $(document).keypress(function () {
+//   $("h1").text("Level");
+// });
