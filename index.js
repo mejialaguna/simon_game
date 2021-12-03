26ff6391e6fa59492e6821976663283e638f104c
Jose Mejia <mejialaguna@yahoo.com>
2021-12-03 16:42:51 -0500

    comparing-memory

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
-const gamePattern = [];
-const userClickedPattern = [];
+var gamePattern = [];
+var userClickedPattern = [];
 const btnColor = ["red", "blue", "green", "yellow"];
+var started = false;
+var level = 0;
+
+
+$(document).keypress(function () {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+
+
+$(".btn").click(function () {
+  const userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  // console.log(userClickedPattern);
+  playSound(userChosenColor);
+  animePress(userChosenColor);
+   checkAnswer(userClickedPattern.length - 1);
+});
+
+
+
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+     if (userClickedPattern.length === gamePattern.length) {
+       setTimeout(function() {
+         nextSequence();
+       }, 1000);
+     }
+  } else {
+    console.log("wrong");
+  }
+}
+
+
+
 
 function nextSequence() {
+  //reset empty array for next level
+
+ userClickedPattern = [];
+
+  level++;  
+  $("h1").text("Level " + level);
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColor = btnColor[randomNumber];
   gamePattern.push(randomChosenColor);
-  //   console.log(randomNumber, randomChosenColor);
   $("#" + randomChosenColor)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  new Audio(`sounds/${randomChosenColor}.mp3`).play();
+  playSound(randomChosenColor);
 }
-function handler() {
-  $(".btn").click(function () {
-    nextSequence();
-    const userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor)
-    console.log(userClickedPattern)
-});
-}
-handler();
 
-// $(document).keypress(function () {
-//   $("h1").text("Level");
-// });
+
+
+function playSound(name) {
+  new Audio(`sounds/${name}.mp3`).play();
+}
+
+function animePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
